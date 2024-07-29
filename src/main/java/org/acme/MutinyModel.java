ce422d5742251b456dcf7ec18d2a1ebb0a1d07cf
RenatoHioji <okamoto.hioji@gmail.com>
2024-07-28 21:25:04 -0300

    adding lombok to project

---
+++ src/main/java/org/acme/MutinyModel.java
@@ -0,0 +1,14 @@
+package org.acme;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class MutinyModel {
+
+    public String name;
+    public String symbol;
+    public int position;
+}
